d6433f3b0df07802e796340532a992c198894aa4
CPatchane <code@patchane.com>
2024-05-27 00:01:03 +0200

    Try with overrideBrowserslist, that works

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import browserslistConfig from "@getaround-eu/browserslist-config"
 
 export default defineConfig({
   plugins: [pluginReact()],
+  // output: {
+  //   overrideBrowserslist: browserslistConfig,
+  // }
 });
